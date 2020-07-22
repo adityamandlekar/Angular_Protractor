d251420259207f6963bf12b6d34ca5d5afcf94c6
Mandlekar <627894@cognizant.com>
2020-07-22 07:24:56 -0400

    2nd test

---
+++ conf.js
@@ -0,0 +1,17 @@
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    framework: 'jasmine',
+    specs: ['spec.js'],
+    capabilities: {
+      'browserName': 'internet explorer',
+      'platform': 'ANY',
+      'version': '11'
+  },
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    isVerbose:true,
+    includeStackTrace:true
+
+}
+  };
