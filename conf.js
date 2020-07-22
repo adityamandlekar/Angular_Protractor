fe09b89c59ab9020fd745e8c0ed539f64afca85d
Mandlekar <627894@cognizant.com>
2020-07-22 07:06:21 -0400

    Test on IE browser

--- conf.js
+++ conf.js
@@ -2,7 +2,16 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
    // framework: 'jasmine',
     specs: ['todo-spec.js'],
-    /*capabilities: {
-      browserName: 'firefox'
-    }*/
+    capabilities: {
+      'browserName': 'internet explorer',
+      'platform': 'ANY',
+      'version': '11'
+  },
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    isVerbose:true,
+    includeStackTrace:true
+
+}
   };
